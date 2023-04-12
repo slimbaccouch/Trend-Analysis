7271512f488a02923bb1159de783e71bc283f8f2
slimbaccouch <75909979+slimbaccouch@users.noreply.github.com>
2023-04-12 06:17:17 +0100

    Add files via upload

---
+++ youtube_glue_job.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Load the CSV file into a Pandas DataFrame
+data = pd.read_csv('D:\Talan\youtube_api_python\comments_1mGOWxLqcEk_13-03-2023.csv')
+
+# Display the first few rows of the DataFrame
+print(data.head())
